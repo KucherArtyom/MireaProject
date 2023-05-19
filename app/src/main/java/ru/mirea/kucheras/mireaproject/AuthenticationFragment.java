442b6142b71fc14989e21092a4f524513f7def5c
KucherArtyom <artyom.kucher.02@gmail.com>
2023-05-19 22:01:03 +0300

    MireaProject, Prac7

---
+++ app/src/main/java/ru/mirea/kucheras/mireaproject/AuthenticationFragment.java
@@ -0,0 +1,88 @@
+package ru.mirea.kucheras.mireaproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
+import ru.mirea.kucheras.mireaproject.databinding.FragmentAuthenticationBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AuthenticationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AuthenticationFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private FragmentAuthenticationBinding binding;
+    private FirebaseAuth mAuth;
+
+    public AuthenticationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AuthenticationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AuthenticationFragment newInstance(String param1, String param2) {
+        AuthenticationFragment fragment = new AuthenticationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAuthenticationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        // Inflate the layout for this fragment
+        return view;
+
+
+    }
+
+}
